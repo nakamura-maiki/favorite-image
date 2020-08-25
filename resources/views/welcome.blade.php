3301f58bf6a2d584fc0403ed88d1f0f878c31772
siratama <565siratama@gmail.com>
2020-08-25 10:40:57 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div></div>
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the favorite-image</h1>
-        </div>
-    </div>
-    
-     <div class="container text-center">
-            <div class="row">
-                <div class="col-md-12"><i class="far fa-user-circle" style="font-size: 200px"></i></div>
-                 {{-- ユーザ登録ページへのリンク --}}
-                <div class="col-md-12">{!! link_to_route('signup.get', '新規登録の方はこちら', [], ['class' => 'nav-link']) !!}</div>
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the favorite-image</h1>
             </div>
         </div>
+        
+         <div class="container text-center">
+                <div class="row">
+                    <div class="col-md-12"><i class="far fa-user-circle" style="font-size: 200px"></i></div>
+                    <div class="col-md-12">{!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-lg btn-primary']) !!}</div>
+                     {{-- ユーザ登録ページへのリンク --}}
+                    <div class="col-md-12">{!! link_to_route('signup.get', '新規登録の方はこちら', [], ['class' => 'nav-link']) !!}</div>
+                </div>
+            </div>
+    @endif
 @endsection
+
+{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}
